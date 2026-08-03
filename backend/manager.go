1975253820d9bf457eda2c63f603799274e59e8b
aniball <kakillama@gmail.com>
2026-08-02 20:04:59 -0400

    Creacion del modulo data_user para obtener los datos de un usuario

--- backend/manager.go
+++ backend/manager.go
@@ -8,11 +8,13 @@ import (
 type BackendManager struct {
 	Login    *services.LoginService
 	Register *services.RegisterService
+	DataUser *services.UserService
 }
 
 func NewBackendManager(db *database.MemoryDB) *BackendManager {
 	return &BackendManager{
 		Login:    services.NewLoginService(db),
 		Register: services.NewRegisterService(db),
+		DataUser: services.NewUserService(db),
 	}
 }
